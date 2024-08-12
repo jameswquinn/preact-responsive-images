c0d7ef1f8347ee69f642ef2121176cc0a4b23605
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-13 00:15:55 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-  
+import { h, render } from 'preact';
+import ResponsiveImage from './components/ResponsiveImage';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to our Preact application with responsive images.</p>
+      <ResponsiveImage src="large_original.png" alt="Responsive Image" />
+    </div>
+  );
+}
+
+render(<App />, document.body);
